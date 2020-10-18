a0c15acbf9ccf67e7736b34a47e22cd49f82dc88
Bhagyashree Sahoo <bhagysah@amdocs.com>
2020-10-18 13:03:05 +0530

    final changes to trivia-api for testcases

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -33,6 +33,124 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_get_all_categories(self):
+        response = self.client().get('/categories')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['categories'])
+
+    def test_invalid_category_id(self):
+        response = self.client().get('/categories/18543/questions')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Unprocessable entity')
+
+    def test_get_all_questions(self):
+        response = self.client().get('/categories')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['questions'])
+
+
+    def test_invalid_page_request(self):
+        response = self.client().get('/questions?page=999')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Resource not found')
+
+    def test_success_delete_question(self):
+        question = Question(question=self.new_question['question'], 
+                            answer=self.new_question['answer'],
+                            category=self.new_question['category'], 
+                            difficulty=self.new_question['difficulty'])
+        question.insert()
+        qid = question.id
+        questions_before_delete = Question.query.all()
+
+        response = self.client().delete('/questions/{}'.format(qid))
+        data = json.loads(response.data)
+        questions_after_delete = Question.query.all()
+
+        question = Question.query.filter(Question.id == 1).one_or_none()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['deleted'], qid)
+        self.assertTrue(len(questions_before_delete) - len(questions_after_delete) == 1)
+        self.assertEqual(question, None)
+
+    def test_invalid_delete_question_request(self):
+        # this tests delete with invalid id
+        response = self.client().delete('/questions/oju567')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Resource not found')
+
+    def test_success_create_new_question(self):
+        questions_before_post = Question.query.all()
+
+        response = self.client().post('/questions', json=self.new_question)
+        data = json.loads(response.data)
+
+        questions_after_post = Question.query.all()
+        question = Question.query.filter_by(id=data['created']).one_or_none()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(questions_after_post) - len(questions_before_post) == 1)
+        self.assertIsNotNone(question)
+
+    def test_invalid_create_question(self):
+        request = {
+            'question': '',
+            'answer': '',
+            'difficulty': 2,
+            'category': 1,
+        }
+        response = self.client().post('/questions', json=request)
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Unprocessable Entity')
+
+    def test_search_for_question(self):
+        request_data = {
+            'searchTerm': 'Donald Trump',
+        }
+        response = self.client().post('/questions/search', json=request_data)
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+    def test_invalid_search_response(self):
+        request_data = {
+            'searchTerm': '',
+        }
+        response = self.client().post('/questions/search', json=request_data)
+        data = json.loads(response.data)
+        
+        self.assertEqual(response.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Unprocessable entity')
+
+    
+
+    
+
+
+    
 
 
 # Make the tests conveniently executable
